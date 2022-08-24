b752185664fc2f220c92bb068df58835d0e17f6c
Tom Harris <76956047+thomas-tank-corp@users.noreply.github.com>
2022-08-24 15:00:45 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -57,32 +57,3 @@ output "k8_dev_cluster_master_endpoint" {
     value = google_container_cluster.dev.endpoint
 }
 
-resource "google_sql_database_instance" "dev" {
-  name             = "humanitec-psql-dev"
-  database_version = "POSTGRES_14"
-  region           = "europe-west2"
-
-  settings {
-    tier = "db-f1-micro"
-  }
-}
-
-
-resource "random_id" "db_pass" {
-  byte_length = 16
-}
-
-resource "google_sql_user" "users" {
-  name     = "humanitec_user"
-  instance = google_sql_database_instance.dev.name
-  password = random_id.db_pass.hex
-}
-
-
-output "psql_dev_connection_name" {
-    value = google_sql_database_instance.dev.connection_name
-}
-
-output "psql_dev_password" {
-    value = random_id.db_pass.hex
-}
