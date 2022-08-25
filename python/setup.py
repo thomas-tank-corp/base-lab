e89a486aca663c229cb5cca63eb5f63f1a9dd5e5
Tom Harris <76956047+thomas-tank-corp@users.noreply.github.com>
2022-08-25 17:55:16 +0100

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 import requests
 import os
 import sys
@@ -19,11 +18,14 @@ try:
     gcp_sa = os.environ['GCP_SA']
     k8_lb_ip = os.environ['K8_DEV_INGRESS_IP']
     k8_gcpprojectid = os.environ ['INSTRUQT_GCP_PROJECT_GCP_PROJECT_PROJECT_ID']
-    k8_gcpzone = os.environ ['TF_VAR_GCP_ZONE']
+    k8_gcpzone = os.environ ['GOOGLE_ZONE']
+    sql_usr = os.environ ['SQL_USR']
+    sql_pass = os.environ ['SQL_PASS']
+    sql_connection = os.environ['SQL_CONNECTION']
 except Exception as e:
     print(f"Error: Could not read {e} from environment.")
     print(f"Please export {e} as environment variable.")
-    sys.exit()
+
 
 humanitec_url = "api.humanitec.io"
 
@@ -79,24 +81,57 @@ else:
 
 # Register GKE Cluster
 ##########################################################
-url = f"https://{humanitec_url}/orgs/{humanitec_org}/resources/definitions"
+# url = f"https://{humanitec_url}/orgs/{humanitec_org}/resources/definitions"
+# payload = {
+#     "org_id": f"{humanitec_org}",
+#     "id": f"k8-dev-{gcp_id}",
+#     "name": f"k8-dev-{gcp_id}",
+#     "type": "k8s-cluster",
+#     "driver_type": "humanitec/k8s-cluster-gke",
+#     "driver_inputs": {
+#       "values": {
+#         "loadbalancer": f"{k8_lb_ip}",
+#         "name": "humanitec-k8-dev",
+#         "project_id": f"{k8_gcpprojectid}",
+#         "zone": f"{k8_gcpzone}"
+#       },
+#       "secrets" : {
+#         "credentials" f"{gcp_sa}"
+#       }
+#     }
+# }
+
+# response = requests.request("POST", url, headers=headers, json=payload)
+# if response.status_code==200:
+#     print(f"The resource definition has been registered.")
+# else:
+#     print(f"Unable to create resource account. POST {url} returned status code {response.status_code}.")
+
+
+
+
+# Register CloudSQL
+##########################################################
+
+url = f"https://{humanitec_url}/orgs/{humanitec_org}/resources/defs"
 payload = {
-    "org_id": f"{humanitec_org}",
-    "id": "kube-gcp-instruqt",
-    "name": "kube-gcp-instruqt",
-    "type": "k8s-cluster",
-    "driver_type": "humanitec/k8s-cluster-gke",
-    "driver_inputs": {
-      "values": {
-        "loadbalancer": f"{k8_lb_ip}",
-        "name": "humanitec-k8-dev",
-        "project_id": f"{k8_gcpprojectid}",
-        "zone": f"{k8_gcpzone}"
-      },
-      "secrets" : {
-        "credentials" f"{gcp_sa}"
-      }
+"criteria": [ ],
+"driver_account": f"{gcp_id}",
+"driver_inputs": {
+ "secrets": {
+    "dbcredentials": {
+      "password": f"{sql_pass}",
+      "username": f"{sql_usr}"
     }
+  },
+  "values": {
+      "instance": f"{sql_connection}"
+  }
+},
+"driver_type": "humanitec/cloudsql",
+"id": f"postgres-dev-{gcp_id}",
+"name": f"postgres-dev-{gcp_id}",
+"type": "postgres"
 }
 
 response = requests.request("POST", url, headers=headers, json=payload)
