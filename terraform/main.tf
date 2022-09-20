893fa9db0beff8a647f4ea068084711b5d8ae601
Tom Harris <76956047+thomas-tank-corp@users.noreply.github.com>
2022-09-20 13:49:11 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,31 +16,31 @@ resource "random_id" "db_pass" {
   byte_length = 16
 }
 
-# resource "google_sql_database_instance" "dev" {
-#   name             = "humanitec-psql-dev"
-#   database_version = "POSTGRES_14"
-#   region           = "europe-west2"
+resource "google_sql_database_instance" "dev" {
+  name             = "humanitec-psql-dev"
+  database_version = "POSTGRES_14"
+  region           = "europe-west2"
 
-#   settings {
-#     tier = "db-f1-micro"
-#   }
-# }
-
-
-# resource "google_sql_user" "users" {
-#   name     = "humanitec_user"
-#   instance = google_sql_database_instance.dev.name
-#   password = random_id.db_pass.hex
-# }
+  settings {
+    tier = "db-f1-micro"
+  }
+}
 
 
-# output "psql_dev_connection_name" {
-#     value = google_sql_database_instance.dev.connection_name
-# }
+resource "google_sql_user" "users" {
+  name     = "humanitec_user"
+  instance = google_sql_database_instance.dev.name
+  password = random_id.db_pass.hex
+}
 
-# output "psql_dev_password" {
-#     value = random_id.db_pass.hex
-# }
+
+output "psql_dev_connection_name" {
+    value = google_sql_database_instance.dev.connection_name
+}
+
+output "psql_dev_password" {
+    value = random_id.db_pass.hex
+}
 
 resource "google_container_cluster" "dev" {
   name               = "humanitec-k8-dev"
