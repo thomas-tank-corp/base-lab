76b05ecde68df19366b748994acb662c3f305586
thomas-tank-corp <tgeez@protonmail.com>
2022-09-22 09:09:05 +0100

    DB

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -66,7 +66,7 @@ module "eks" {
       max_size     = 3
       desired_size = 3
 
-      instance_types = ["t3.large"]
+      instance_types = ["t3.small"]
       capacity_type  = "SPOT"
     }
   }
@@ -76,4 +76,56 @@ module "eks" {
     Environment = "dev"
     Terraform   = "true"
   }
+}
+
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 4.0"
+
+  name        = local.name
+  description = "Complete PostgreSQL example security group"
+  vpc_id      = module.vpc.vpc_id
+
+  # ingress
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 5432
+      to_port     = 5432
+      protocol    = "tcp"
+      description = "PostgreSQL access from within VPC"
+      cidr_blocks = module.vpc.vpc_cidr_block
+    },
+  ]
+
+  tags = local.tags
+}
+
+
+module "db" {
+  source  = "terraform-aws-modules/rds/aws"
+  version = "5.1.0"
+
+  identifier = RDS_POSTGRES
+
+
+  engine               = "postgres"
+  engine_version       = "14.1"
+  family               = "postgres14" 
+  major_engine_version = "14"         
+  instance_class       = "db.t4g.large"
+
+  allocated_storage     = 20
+  max_allocated_storage = 100
+
+  # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
+  # "Error creating DB Instance: InvalidParameterValue: MasterUsername
+  # user cannot be used as it is a reserved word used by the engine"
+  db_name  = RDS_POSTGRES
+  username = "postgresql"
+  port     = 5432
+
+  multi_az               = true
+  db_subnet_group_name   = module.vpc.vpc_id
+  vpc_security_group_ids = [module.security_group.security_group_id]
+
 }
