2ce5510056aadac6488d7bcb8f0503e11d93e82e
thomas-tank-corp <tgeez@protonmail.com>
2022-09-28 11:19:20 +0100

    +

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -57,16 +57,14 @@ module "eks" {
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     disk_size      = 50
-    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
+  
   }
 
   eks_managed_node_groups = {
-    blue = {}
-    green = {
+    primary = {
       min_size     = 1
       max_size     = 3
-      desired_size = 3
-
+      desired_size = 1
       instance_types = ["t3.small"]
       capacity_type  = "SPOT"
     }
