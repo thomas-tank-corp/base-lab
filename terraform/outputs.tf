01748b6effa70906426879fe1c0d0f0f841796cd
Tom Harris <76956047+thomas-tank-corp@users.noreply.github.com>
2022-09-30 15:54:10 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,13 +2,13 @@ output "eks_cluster_endpoint" {
     value = module.eks.cluster_endpoint
 }
 
-# output "psql_dev_connection_name" {
-#     value = google_sql_database_instance.dev.connection_name
-# }
+output "psql_dev_connection_name" {
+    value = google_sql_database_instance.dev.connection_name
+}
 
-# output "psql_dev_password" {
-#     value = random_id.db_pass.hex
-# }
+output "psql_dev_password" {
+    value = random_id.db_pass.hex
+}
 
 
 output "k8_dev_cluster_id" {
