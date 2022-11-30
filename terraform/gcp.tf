007afa3332446804fd99c2631ba5e30bc5d41803
Tom Harris <76956047+thomas-tank-corp@users.noreply.github.com>
2022-11-30 10:31:43 +0000

    Update gcp.tf

--- terraform/gcp.tf
+++ terraform/gcp.tf
@@ -21,10 +21,4 @@ resource "google_sql_user" "users" {
   password = random_id.db_pass.hex
 }
 
-resource "google_container_cluster" "dev" {
-  name               = "humanitec-k8-dev"
-  location           = "europe-west2-a"
-  initial_node_count = 1
-}
-
 
